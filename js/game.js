31454564875ec8f0b362adf79d9e5a06f0233136
huddinger <huddinger@posteo.net>
2022-09-25 21:54:59 +0200

    refactored js

--- js/game.js
+++ js/game.js
@@ -1,153 +1,192 @@
 var structureStats = {
   treehouse : {
+    maxBuilders: 3,
     maxHealth: 10,
     repairable: true
   }
 }
 
+
+
 class Structure {
+  id;
   type;
   health;
-  boundKapas;
+  progress;
+  builders;
 
-  constructor(type) {
-    if (type in structureStats) {
-      this.type = type
-      var stats = structureStats[type]
-      this.health = 0
-      this.boundKapas = 0
-    } else {
-      throw new Error("Structure ${type} not recognized")
-    }
+  constructor (id) {
+    this.id = id
+    this.health = 0
+    this.progress = 0
+    this.builders = 0
   }
 
   build() {
-    var maxHealth = structureStats[this.type].maxHealth
-    var health = this.health
-    if (health < maxHealth && this.boundKapas > 0) {
-      health += this.boundKapas
-      if (health > maxHealth) {
-        health = maxHealth
-      }
-      this.health = health
-    }
-    if (health == maxHealth) {
-      this.boundKapas = 0
-    }
+    let maxHealth = structureStats[this.type].maxHealth
+
+    // make progress
+    this.health += this.builders
+
+    // finished
+    if (this.health >= maxHealth)
+      this.health = maxHealth
+
+    this.progress = Math.round((this.health / maxHealth) * 100)
   }
 }
 
-var structures = []
-
-activistBase = 10
-activistCount = activistBase
-
-// capacities
-capacities = {
-    repro: {
-      boundKapas: 0
-    }
+class Treehouse extends Structure {
+  constructor(id) {
+    super(id)
+    this.type = "treehouse"
+  }
 }
 
-// returns the current activist limit
-getActivistLimit = function() {
-  return activistBase + capacities.repro.boundKapas * 7
+
+//
+// Having all structures defined
+// we can build our village
+//
+
+
+//
+// Important note:
+// Builder management is done by the village class
+//
+class Village {
+  nextStructureId;
+  structures;
+  villagers;
+
+  constructor() {
+    this.structures = {}
+    this.nextStructureId = 0
+
+    this.villagers = {
+      baseCapacity : 10,
+      capacity: 10,
+      total: 10,
+      free: 10,
+      support: 0,
+      builders: 0
+    }
+  }
+
+  structureCount(type) {
+    let n = 0
+    for (let id in this.structures)
+      if (this.structures[id].type == type)
+        n++
+    return n
+  }
+
+  assignBuilders(id) {
+    let struct = this.structures[id]
+    let max = structureStats[struct.type].maxBuilders
+    let req = max - struct.builders
+
+    let available = this.villagers.free
+
+    //how many additional builders can we mobilize?
+    let add = available < req ? available : req
+
+    struct.builders += add
+    this.villagers.free -= add
+    this.villagers.builders += add
+  }
+
+  releaseBuilders(id) {
+    let n = this.structures[id].builders
+    this.structures[id].builders = 0
+    this.villagers.free += n
+    this.villagers.builders -= n
+  }
+
+  build() {
+    for (let id in this.structures) {
+      this.structures[id].build()
+      if (this.structures[id].progress == 100 && this.structures[id].builders > 0)
+        this.releaseBuilders(id)
+    }
+  }
+
+  addTreehouse() {
+    // technical stuff
+    let id = this.nextStructureId++
+    let treehouse = new Treehouse(id)
+    this.structures[id] = treehouse
+    this.assignBuilders(id)
+  }
+
+  increaseSupport() {
+    var v = this.villagers
+
+    if (v.free == 0)
+      return
+
+    v.support++
+    v.free--
+    v.capacity = v.baseCapacity + 7 * v.support
+  }
+
+  recruitVillagers() {
+    let v = this.villagers
+    let add = Math.floor((v.capacity - v.total) / 3)
+    v.total += add
+    v.free += add
+  }
+}
+
+
+
+game = {
+  timeframe: 0,
+  isFinished: false,
+  loopId: undefined,
+  village: new Village()
 }
 
 //
-recruitActivists = function() {
-  limit = getActivistLimit()
-  add = Math.floor()(limit - activistCount) / 3)
-  activistCount = activistCount + add
-}
+// UI Functions
+//
 
-boundInCapacities = function() {
-  var n = 0
-  for (c in capacities) {
-    n += capacities[c].boundKapas
-  }
-  return n
-}
-
-boundInBuilding = function() {
-  var n = 0
-  for (s in structures) {
-    n += structures[s].boundKapas
-  }
-  return n
-}
-
-freeActivists = function() {
-  return activistCount - (boundInCapacities() + boundInBuilding())
-}
-
-buildTreehouse = function() {
-  treehouse = new Structure("treehouse")
-  freeKapas = freeActivists()
-  if (freeActivists < 3) {
-    treehouse.boundKapas = freeActivists
-  } else {
-    treehouse.boundKapas = 3
-  }
-  structures.push(treehouse)
-}
-
-treehouseCount = function() {
-  n = 0
-  for (s of structures) {
-    if (s.type == "treehouse") {
-      n++
-    }
-  }
-  return n
-}
-
-increaseRepro = function() {
-  if (freeActivists() > 0) {
-    capacities.repro.boundKapas++
-  }
-}
-
-debugStructures = function() {
-  console.log(structures)
+uihandlers = {
+  increaseSupport : function(e) {game.village.increaseSupport()},
+  addTreehouse: function(e) {game.village.addTreehouse()}
 }
 
 updateUI = function() {
-  document.getElementById("activist-count").innerHTML = activistCount
-  document.getElementById("free-activists").innerHTML = freeActivists()
-  document.getElementById("capacity-count").innerHTML = boundInCapacities()
-  document.getElementById("build-count").innerHTML = boundInBuilding()
-  document.getElementById("treehouse-count").innerHTML = treehouseCount()
-
+  let vs = game.village.villagers
+  document.getElementById("game-timeframe").innerHTML = game.timeframe
+  document.getElementById("vs-capacity").innerHTML = vs.capacity
+  document.getElementById("vs-villagers").innerHTML = vs.total
+  document.getElementById("vs-free").innerHTML = vs.free
+  document.getElementById("vs-repro").innerHTML = vs.support
+  document.getElementById("vs-builders").innerHTML = vs.builders
+  document.getElementById("struct-treehouse-count").innerHTML = game.village.structureCount("treehouse")
 }
+
 
 bindUI = function() {
-  document.getElementById("increase-repro").onclick = increaseRepro
-  document.getElementById("build-treehouse").onclick = buildTreehouse
+  document.getElementById("action-increase-repro").addEventListener("click", uihandlers.increaseSupport)
+  document.getElementById("action-build-treehouse").addEventListener("click", uihandlers.addTreehouse)
 }
 
-gameFinished = false
-gameLoop = 0
+function loop() {
+  game.timeframe++
 
-loop = function() {
-  console.log("loop")
-  console.log(capacities)
-  console.log(structures)
-  if (gameFinished) {
-    clearInterval(gameLoop)
-    return
-  }
-
-  recruitActivists()
-
-  // Build process
-  for (s of structures) {
-    s.build()
-  }
+  v = game.village
+  v.recruitVillagers()
+  v.build()
 
   updateUI()
+
+  if (game.isFinished)
+    clearInterval(game.loopId)
 }
+
 
 bindUI()
 loop()
-gameLoop = window.setInterval(loop, 2000)
+game.loopId = window.setInterval(loop, 2000)
